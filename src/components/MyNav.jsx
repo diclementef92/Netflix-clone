78c81de27a21b53a42e6fb9db598d47a5ddc2566
diclementef92 <diclementef92@gmail.com>
2023-07-24 19:59:13 +0200

    new nav and tvshows page

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -1,59 +1,72 @@
 import { Component } from "react";
-import { Nav } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 class MyNav extends Component {
   render() {
     return (
-      <Nav className="navbar navbar-expand-lg navbar-dark">
-        <a className="navbar-brand" href="#">
-          <img src="/assets/img/logo.png" />
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src="/assets/img/logo.png" />
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="./home">Home</Nav.Link>
+            <Nav.Link href="./tvshows">Tv Shows</Nav.Link>
+            <Nav.Link href="./movies">Movies</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      // <Navbar bg="dark" data-bs-theme="dark">
+      // <Container>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <a className="nav-link " href="/">
-                Home <span className="sr-only"></span>
-              </a>
-            </li>
-            <li className="nav-item ">
-              <a className="nav-link active" href="#">
-                TV Shows
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link " href="./movies">
-                Movies
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link " href="#">
-                Recently Added
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link " href="#">
-                My List
-              </a>
-            </li>
-          </ul>
-          <i className="bi bi-search icons"></i>
-          <div id="kids">KIDS</div>
-          <i className="bi bi-bell icons"></i>
-          <i className="bi bi-user icons"></i>
-        </div>
-      </Nav>
+      //     <img src="/assets/img/logo.png" />
+      //   </a>
+      //   <button
+      //     className="navbar-toggler"
+      //     type="button"
+      //     data-toggle="collapse"
+      //     data-target="#navbarSupportedContent"
+      //     aria-controls="navbarSupportedContent"
+      //     aria-expanded="false"
+      //     aria-label="Toggle navigation"
+      //   >
+      //     <span className="navbar-toggler-icon"></span>
+      //   </button>
+
+      //   <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      //     <ul className="navbar-nav mr-auto">
+      //       <li className="nav-item">
+      //         <a className="nav-link " href="/">
+      //           Home <span className="sr-only"></span>
+      //         </a>
+      //       </li>
+      //       <li className="nav-item ">
+      //         <a className="nav-link" href="#">
+      //           TV Shows
+      //         </a>
+      //       </li>
+      //       <li className="nav-item">
+      //         <a className="nav-link active" href="./movies">
+      //           Movies
+      //         </a>
+      //       </li>
+      //       <li className="nav-item">
+      //         <a className="nav-link " href="#">
+      //           Recently Added
+      //         </a>
+      //       </li>
+      //       <li className="nav-item">
+      //         <a className="nav-link " href="#">
+      //           My List
+      //         </a>
+      //       </li>
+      //     </ul>
+      //     <i className="bi bi-search icons"></i>
+      //     <div id="kids">KIDS</div>
+      //     <i className="bi bi-bell icons"></i>
+      //     <i className="bi bi-user icons"></i>
+      //   </div>
+      // </Nav>
     );
   }
 }
---
+++ src/components/TvShows.jsx
@@ -0,0 +1,46 @@
+import { Component } from "react";
+import { Col, Row } from "react-bootstrap";
+import MoviePicture from "./MoviePicture";
+
+class TvShows extends Component {
+  state = {
+    tvshows: [],
+  };
+
+  async componentDidMount() {
+    try {
+      let res = await fetch(
+        "http://www.omdbapi.com/?apikey=1bbfedb5&s=" +
+          this.props.search +
+          "&type=series"
+      );
+      if (res.ok) {
+        const body = await res.json();
+        this.setState({ tvshows: body.Search });
+      } else {
+        console.log("Error in fetch: response status", res.status);
+      }
+    } catch (error) {
+      console.log("Error in fetch: ", error);
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <h2 className="text-light mt-4">{this.props.search}</h2>
+        <Row>
+          {this.state.tvshows.slice(0, 4).map((movie) => {
+            return (
+              <Col xs={12} sm={6} md={4} lg={3} key={movie.imdbID}>
+                <MoviePicture src={movie.Poster}></MoviePicture>
+              </Col>
+            );
+          })}
+        </Row>
+      </>
+    );
+  }
+}
+
+export default TvShows;
