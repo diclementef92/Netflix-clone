4056f7460200439e9ca8a7d2a93b7562e264522d
diclementef92 <diclementef92@gmail.com>
2023-07-25 12:48:06 +0200

    nav links colors

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -4,15 +4,21 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 class MyNav extends Component {
   render() {
     return (
-      <Navbar bg="dark" data-bs-theme="dark">
+      <Navbar bg="dark" data-bs-theme="dark" activeKey="Home">
         <Container>
           <Navbar.Brand href="#home">
             <img src="/assets/img/logo.png" />
           </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="./home">Home</Nav.Link>
-            <Nav.Link href="./tvshows">Tv Shows</Nav.Link>
-            <Nav.Link href="./movies">Movies</Nav.Link>
+            <Nav.Link href="./home" eventKey="Home">
+              Home
+            </Nav.Link>
+            <Nav.Link href="./tvshows" eventKey="Tv Shows">
+              Tv Shows
+            </Nav.Link>
+            <Nav.Link href="./movies" eventKey="Movies">
+              Movies
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
