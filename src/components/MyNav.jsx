4806aceefb608f3d2578ad89044e09ac49a20b58
diclementef92 <diclementef92@gmail.com>
2023-07-26 18:16:27 +0200

    remove comments

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -22,57 +22,6 @@ class MyNav extends Component {
           </Nav>
         </Container>
       </Navbar>
-      // <Navbar bg="dark" data-bs-theme="dark">
-      // <Container>
-
-      //     <img src="/assets/img/logo.png" />
-      //   </a>
-      //   <button
-      //     className="navbar-toggler"
-      //     type="button"
-      //     data-toggle="collapse"
-      //     data-target="#navbarSupportedContent"
-      //     aria-controls="navbarSupportedContent"
-      //     aria-expanded="false"
-      //     aria-label="Toggle navigation"
-      //   >
-      //     <span className="navbar-toggler-icon"></span>
-      //   </button>
-
-      //   <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      //     <ul className="navbar-nav mr-auto">
-      //       <li className="nav-item">
-      //         <a className="nav-link " href="/">
-      //           Home <span className="sr-only"></span>
-      //         </a>
-      //       </li>
-      //       <li className="nav-item ">
-      //         <a className="nav-link" href="#">
-      //           TV Shows
-      //         </a>
-      //       </li>
-      //       <li className="nav-item">
-      //         <a className="nav-link active" href="./movies">
-      //           Movies
-      //         </a>
-      //       </li>
-      //       <li className="nav-item">
-      //         <a className="nav-link " href="#">
-      //           Recently Added
-      //         </a>
-      //       </li>
-      //       <li className="nav-item">
-      //         <a className="nav-link " href="#">
-      //           My List
-      //         </a>
-      //       </li>
-      //     </ul>
-      //     <i className="bi bi-search icons"></i>
-      //     <div id="kids">KIDS</div>
-      //     <i className="bi bi-bell icons"></i>
-      //     <i className="bi bi-user icons"></i>
-      //   </div>
-      // </Nav>
     );
   }
 }
