8abbb1204ff3b1ae65daf13c6c9743cdd08a5d96
diclementef92 <diclementef92@gmail.com>
2023-08-07 16:30:22 +0200

    search bar

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -1,25 +1,41 @@
 import { Component } from "react";
-import { Container, Image, Nav, Navbar } from "react-bootstrap";
+import { Container, Image, InputGroup, Nav, Navbar } from "react-bootstrap";
+import { FiSearch } from "react-icons/fi";
 
 class MyNav extends Component {
   render() {
     return (
-      <Navbar bg="dark" data-bs-theme="dark" activekey="Home">
+      <Navbar expand="lg" bg="dark" data-bs-theme="dark" activekey="Home">
         <Container>
           <Navbar.Brand href="#home">
             <Image src="/assets/img/logo.png" />
           </Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="./home" eventKey="Home">
-              Home
-            </Nav.Link>
-            <Nav.Link href="./tvshows" eventKey="Tv Shows">
-              Tv Shows
-            </Nav.Link>
-            <Nav.Link href="./movies" eventKey="Movies">
-              Movies
-            </Nav.Link>
-          </Nav>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="./home" eventKey="Home">
+                Home
+              </Nav.Link>
+              <Nav.Link href="./tvshows" eventKey="Tv Shows">
+                Tv Shows
+              </Nav.Link>
+              <Nav.Link href="./movies" eventKey="Movies">
+                Movies
+              </Nav.Link>
+            </Nav>
+            <div class="input-group search-bar">
+              <input
+                class="form-control border-secondary"
+                type="seach"
+                value="search"
+              />
+              <div class="input-group-append">
+                <button class="btn btn-outline-secondary" type="button">
+                  <FiSearch />
+                </button>
+              </div>
+            </div>
+          </Navbar.Collapse>{" "}
         </Container>
       </Navbar>
     );
