824eaecf76dbd98ede567a6228ecf5e295e7f8be
diclementef92 <diclementef92@gmail.com>
2023-09-20 11:52:25 +0200

    ~Home

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -21,13 +21,13 @@ const MyNav = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="./home" eventKey="Home">
+            <Nav.Link href="~/home" eventKey="Home">
               Home
             </Nav.Link>
             <Link to="./tvshows" eventKey="Tv Shows">
               Tv Shows
             </Link>
-            <Nav.Link href="./movies" eventKey="Movies">
+            <Nav.Link href="~/movies" eventKey="Movies">
               Movies
             </Nav.Link>
           </Nav>
